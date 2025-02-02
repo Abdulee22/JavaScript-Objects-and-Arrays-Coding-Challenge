e9bba8d16ef65053ed121e096a3ed43762c1e11a
Abdulhakim <Abdulthe383@gmail.com>
2025-02-02 00:31:31 -0500

    Task 2

--- cc_3.js
+++ cc_3.js
@@ -3,3 +3,11 @@ let prices = [5,7,8,9,3];
 prices.push(4);
 prices.shift();
 console.log( "new prices", prices);
+
+// task 2  processing customer orders and updating order quantities.//
+
+let orders = [6,8,3,4,9];
+orders[2]+=5;
+let totalorders = orders.reduce((sum,qty) => sum+qty,0);
+console.log("updated orders",orders);
+console.log ("total order count",totalorders); 
